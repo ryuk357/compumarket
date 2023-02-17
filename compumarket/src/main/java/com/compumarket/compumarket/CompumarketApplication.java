267baa7a26f52526f57e2d117ec1ed619f7da224
kilianGitB <79051821+KilianGitB@users.noreply.github.com>
2023-02-17 23:03:18 +0100

    first commit

---
+++ compumarket/src/main/java/com/compumarket/compumarket/CompumarketApplication.java
@@ -0,0 +1,13 @@
+package com.compumarket.compumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompumarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompumarketApplication.class, args);
+	}
+
+}
