595b60ae679a9b485d82d65ff1d869261acdfb6f
kilianGitB <79051821+KilianGitB@users.noreply.github.com>
2023-02-21 13:32:26 +0100

    Ajout password + contraintes

--- compumarket/src/main/java/com/compumarket/compumarket/entities/Person.java
+++ compumarket/src/main/java/com/compumarket/compumarket/entities/Person.java
@@ -1,13 +1,19 @@
 package com.compumarket.compumarket.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 public abstract class Person {
 	//attributes
-	
+	@Column(nullable = false)
+	private String login;
+	@Column(nullable = false)
 	private String firstName;
+	@Column(nullable = false)
 	private String lastName;
+	@Column(nullable = false)
+	private String pwd;
 	
 	
 	//getters & setters
@@ -26,5 +32,13 @@ public abstract class Person {
 	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	public String getPwd() {
+		return pwd;
+	}
+	
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
 	}
 }
